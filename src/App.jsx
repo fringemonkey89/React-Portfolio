55aeedca94c5e17f393774e80c7fc25a62dd1a7c
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-26 11:12:32 -0500

    added initial files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import About from './components/About/About'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <About />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/About/About.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+    return (
+        <section id='About'>
+            <h5>Who Is Marko?</h5>
+            <h2>About Me</h2>
+
+            <div className='about_container'>
+                <div className='about_me'>
+                    <div className='about_image'>
+                        <img></img>
+                    </div>
+                </div>
+
+                <div className='about_content'>
+                    <div className='about_cards'>
+                        <article className='about_card'>
+                            
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default About
