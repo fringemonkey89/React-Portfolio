6474460f5fa527a735d50ab1a85360e5f8c20c14
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-26 15:07:18 -0500

    added more parts to footer and about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './About.css'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
     return (
@@ -17,9 +20,26 @@ const About = () => {
                 <div className='about_content'>
                     <div className='about_cards'>
                         <article className='about_card'>
-                            
+                            <FaAward className='about_icon' />
+                            <h5>Non-Profit Expereince</h5>
+                            <small>5 Years Experience</small> 
+                        </article>
+
+                        <article className='about_card'>
+                            <FiUsers className='about_icon' />
+                             <h5>Industries</h5>
+                            <small>Construction, Non-Profit, Retail</small> 
+                        </article>
+
+                        <article className='about_card'>
+                            <VscFolderLibrary className='about_icon' />
+                             <h5>Projects</h5>
+                            <small>Take a Gander at My Projects</small> 
                         </article>
                     </div>
+                    <p> Full Stack Developer who is familiarly acqainted with the non-profit sector</p>
+                    <p>I've always been passionate about web development</p>
+                    <a href='#contact' className='btn btn-primary'>Contact</a>
                 </div>
             </div>
         </section>
