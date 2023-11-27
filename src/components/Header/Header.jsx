bade89f2324f7fb43e564dcd8ab81b75987e97a6
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-27 13:12:52 -0500

    re-arranged header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import './Header.css'
-import Resume from './Resume'
 import HeaderSocial from './HeaderSocial'
-
+import Logo  from '../../assets/logo.png'
 const Header = () => {
     return (
         <header>
             <div className='header_container'>
                 <div className='profile'>
                     Hi! My Name is Marko Majetic 
+                    
                      I'm a Fullstack Developer 
                 </div>
+                <div className='logo'>
+                    <img src={Logo} />
+                </div>
                 <HeaderSocial />
-                <Resume />
-                
+               
                 
             </div>
         </header>
